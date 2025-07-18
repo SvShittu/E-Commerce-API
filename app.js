345831c3b1b5039484750473b955adfdebe5daa9
Sarah Victoria Shittu <svshittu@gmail.com>
2025-07-18 17:25:57 +0600

    Parse Cookies Setup

--- app.js
+++ app.js
@@ -5,15 +5,20 @@ const dotenv = require("dotenv").config()
 const connectDB = require("./db")
 const app = express()
 const morgan = require("morgan")
+const cookieParser = require("cookie-parser")
 const authRoutes = require("./routes/authRoutes")
 app.use(morgan("tiny"))
 app.use(express.json())
+app.use(cookieParser())
 
 
 connectDB()
 // middleware
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
+    res.send("e-commerce api")
+})
+app.get("/api/v1", (req, res)=>{
+    console.log(req.cookies)
     res.send("e-commerce api")
 }) 
 
