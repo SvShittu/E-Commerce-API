1fa8ae36f4769a30103ad5fb8760246b5bb8c085
Sarah Victoria Shittu <svshittu@gmail.com>
2025-08-26 19:00:32 +0600

    updated

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000
 const start = async() => {
     try {
         
-        app.listen(PORT, console.log(`Server is running on port ${PORT}...`))
+        app.listen(PORT, console .log(`Server is running on port ${PORT}...`))
     } catch (error) {
     console.log(error)
     }
